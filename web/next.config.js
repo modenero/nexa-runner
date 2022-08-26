cceef9d070449747482484a265869785cc8fe95d
Ava's DAO <support@avasdao.org>
2022-08-25 21:19:41 -0400

    Add standalone support.

--- web/next.config.js
+++ web/next.config.js
@@ -2,15 +2,16 @@ const withMarkdoc = require('@markdoc/next.js')
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  pageExtensions: ['js', 'jsx', 'md'],
-  experimental: {
-    newNextLinkBehavior: true,
-    scrollRestoration: true,
-    images: {
-      allowFutureImage: true,
+    reactStrictMode: true,
+    pageExtensions: ['js', 'jsx', 'md'],
+    experimental: {
+        newNextLinkBehavior: true,
+        scrollRestoration: true,
+        images: {
+            allowFutureImage: true,
+        },
     },
-  },
+    output: 'standalone',
 }
 
 module.exports = withMarkdoc()(nextConfig)
